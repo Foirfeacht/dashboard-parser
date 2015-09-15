975fd011570970649614077639243438e541df03
Foirfeacht <vitaliy.maltsev@gmail.com>
2015-09-15 19:21:49 +0300

    pdfcrowd

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var pdfkit = require('pdfkit');
+var pdf = require('pdfcrowd');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,7 +14,7 @@ var app = express();
 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
 
 // uncomment after placing your favicon in /public
@@ -28,11 +29,11 @@ app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+});*/
 
 // error handlers
 
@@ -41,22 +42,24 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    /*res.render('error', {
       message: err.message,
       error: err
-    });
+    });*/
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
-});
+});*/
+
+
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var pdf = require('pdfcrowd');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
     //res.sendFile(path.join(__dirname, '../views', 'index.html'));
-    res.sendfile('./views/index.html');
+    res.render('index');
+});
+
+router.get('/print', function(req, res, next) {
+	console.log('start');
+	// create an API client instance
+	var client = new pdf.Pdfcrowd("Vitalym", "5ed3874d8b99f468a8bdc1453fe498d0");
+	console.log(client);
+
+// convert a web page and save it to a file
+	client.convertURI('https://almsaeedstudio.com/AdminLTE', pdf.saveToFile("./public/output/file.pdf"));
 });
 
 module.exports = router;
