fbab5963a663eb09bef57cf7e62dd4dc3b5c646b
Foirfeacht <vitaliy.maltsev@gmail.com>
2015-09-16 13:22:37 +0300

    screencapture

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var pdf = require('pdfcrowd');
+var wkhtmltopdf = require('wkhtmltopdf');
+var phantom = require('phantom');
+//var pdf = require('phantom-html2pdf');
+//var pdf = require('html-pdf');
+var webshot = require('webshot');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,12 +16,34 @@ router.get('/', function(req, res, next) {
 
 router.get('/print', function(req, res, next) {
 	console.log('start');
-	// create an API client instance
-	var client = new pdf.Pdfcrowd("Vitalym", "5ed3874d8b99f468a8bdc1453fe498d0");
-	console.log(client);
+	/*var options = {
+		"html": "https://almsaeedstudio.com/AdminLTE"
+	};
+	pdf.convert(options, function(result) {
+		console.log('started converting');
+		result.toFile("./public/output/out.pdf", function() {});
 
-// convert a web page and save it to a file
-	client.convertURI('https://almsaeedstudio.com/AdminLTE', pdf.saveToFile("./public/output/file.pdf"));
+	});*/
+	/*var page = require('webpage').create();
+	console.log(page);
+	page.open('https://almsaeedstudio.com/AdminLTE', function() {
+		var url = page.url;
+		console.log('URL: ' + url);
+		console.log('started render');
+		page.render('./public/output/out.pdf');
+		phantom.exit();
+	});*/
+	/*pdf.create('https://almsaeedstudio.com/AdminLTE', options).toFile('./public/output/out.pdf', function(err, res) {
+		if (err) return console.log(err);
+		console.log(res); // { filename: '/app/businesscard.pdf' }
+	});*/
+
+	//wkhtmltopdf('https://almsaeedstudio.com/AdminLTE', { output: './public/output/out.pdf' });
+	webshot('https://almsaeedstudio.com/AdminLTE', './public/output/out.pdf', function(err) {
+		// screenshot now saved to google.png
+		console.log('saved');
+	});
+
 });
 
 module.exports = router;
